acebf519ad21d7baa9d5b170c32f25f2ffd9055e
Shadowsith <shadowsith94@gmail.com>
2016-10-22 23:05:40 +0200

    Update

--- textadventure.c
+++ textadventure.c
@@ -3,14 +3,44 @@
 
 int main(void)
 {
-  char Name[100];
+  char name[100];
 
   printf("Gebe deinen Spielername ein: \n");
-  scanf("%s", Name);
-  printf("Hallo %s, Willkommen beim Testadventure \n", Name);
+  scanf("%s", name);						//Zuweisung des Spielernamens (String)
+  printf("Hallo %s, Willkommen beim Testadventure \n", name);
   printf("Wähle bitte eine Spielerklasse aus \n");
-  printf("Krieger (k) \n Bogenschütze (b)\n Magier (m)\n");
+  printf("Krieger (1) \nBogenschütze (2)\nMagier (3)\n");
+  int playerclass;						//RPG-Klassendeklaration
+  int warrior;
+  int archer;
+  int mage;
+  scanf("%i", &playerclass);
+  if ( playerclass == 1)					//Prüfung der RPG-KLasse
+  {
+   printf("Du hast die Klasse Krieger gewählt!\n");
+   playerclass == warrior;
+  }
+  else
+  {
+    if( playerclass == 2)
+    {
+     printf("Du hast die Klasse Bogenschütze gewählt!\n");
+    }
+     else
+     {
+      if ( playerclass == 3)
+      {
+       printf("Du hast die Klasse Magier gewählt! \n");
+      }
+       else
+  	{	
+  	 printf("Du hast eine falsche Eingabe getätigt! \n");
+ 	}
+     }
+     
+   }
   
+
   
 }
 
