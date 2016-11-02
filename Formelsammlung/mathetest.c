b05f6493547c3fe55d54bd477e21846762ea506e
Shadowsith <shadowsith94@gmail.com>
2016-11-02 22:00:48 +0100

    Neue Projekte

---
+++ Formelsammlung/mathetest.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h> // Mathematik Bibliothek einbinden.
+
+int main()
+{
+    int a = 2;
+    int b = 2;
+    int result = pow(a,b);
+    printf("Das Quadrat von 2 ist %d.\n", result);
+    return 0;
+}
