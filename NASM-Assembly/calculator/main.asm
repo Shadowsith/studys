54cab560cc5dd494a77fef67ca88a6b2dd0924c8
Shadowsith <shadowsith94@gmail.com>
2017-01-05 00:22:58 +0100

    new assembly project

---
+++ NASM-Assembly/calculator/main.asm
@@ -0,0 +1,90 @@
+global main
+extern printf
+extern scanf
+
+section .data
+
+begin: db "Please write which simple calculation you want to do:",10,0 
+add_o: db "1 for addition",10,0
+sub_o: db "2 for subtraction",10,0
+mul_o: db "3 for multiply",10,0
+div_o: db "4 for division",10,0
+
+msg: db "%s %s %s %s %s",10,0
+
+x: dd 0
+a: dd 0
+b: dd 0
+
+in: db "%d",0
+out: db "%d",10,0
+
+
+section .text
+
+main:
+
+ push div_o
+ push mul_o
+ push sub_o
+ push add_o
+ push begin
+ push msg
+ call printf
+ add  esp,24
+
+ push x
+ push in
+ call scanf
+ add  esp,8
+ 
+ mov eax,[x]
+ cmp eax,1
+ je  add_ch
+ cmp eax,2
+ je  sub_ch
+ cmp eax,3
+ je  mul_ch
+ cmp eax,4
+ je  div_ch
+ 
+end:
+ 
+ ret
+
+add_ch:
+ 
+ push a
+ push in
+ call scanf
+ add  esp,8
+ 
+ push b
+ push in
+ call scanf
+ add  esp,8
+ 
+ mov eax,[a]
+ add eax,[b]
+
+ push eax
+ push out
+ call printf
+ add  esp,8
+
+ 
+
+ jmp end
+
+sub_ch:
+
+ jmp end
+
+mul_ch:
+
+ jmp end
+
+div_ch:
+
+ jmp end
+
